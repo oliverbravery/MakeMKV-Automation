9d13c488129f8575d0ee050969085039091a7215
oliverbravery <dev@oliverbravery.uk>
2024-01-13 11:39:52 +0000

    Refactor MakeMKVHelper class in dvd_ripper.py

--- dvd_ripper.py
+++ dvd_ripper.py
@@ -5,20 +5,20 @@ from makemkv import MakeMKV
 
 class MakeMKVHelper:
     def __init__(self, drive_path='/dev/sr0'):
-        self.makemkv = MakeMKV(drive_path)
+        self.makemkv: MakeMKV = MakeMKV(drive_path)
 
-    def get_disc_information(self):
+    def get_disc_information(self) -> (str, list):
         disc_info = self.makemkv.info()
-        film_name = disc_info["disc"]["name"]
-        tracks = []
-        count = 0
+        film_name: str = disc_info["disc"]["name"]
+        tracks: list = []
+        count: int = 0
         for k in disc_info["titles"]:
-            track = {"track_number": count, "name": k["file_output"], "bit_size": k["size"],
+            track: dict = {"track_number": count, "name": k["file_output"], "bit_size": k["size"],
                      "human_size": k["size_human"], "duration": k["length"]}
             tracks.append(track)
             count += 1
-        tracks = sorted(tracks, key=lambda d: d['bit_size'], reverse=True)
+        tracks: list = sorted(tracks, key=lambda d: d['bit_size'], reverse=True)
         return film_name, tracks
 
-    def make_movie(self, track_number, save_location):
+    def make_movie(self, track_number: str, save_location: str):
         self.makemkv.mkv(track_number, save_location)
